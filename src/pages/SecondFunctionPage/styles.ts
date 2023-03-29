21142852d00a5955187f3a1462b2a56bc824c8d4
leonardo <web.dev.trabalho@gmail.com>
2023-03-29 20:36:20 -0300

    removing trash code

--- src/pages/SecondFunctionPage/styles.ts
+++ src/pages/SecondFunctionPage/styles.ts
@@ -2,13 +2,6 @@ import styled from 'styled-components';
 
 import {View} from 'react-native';
 
-export const BodyContainer = styled(View)`
-  flex: 1;
-  background-color: #ebebeb;
-  align-items: center;
-  justify-content: center;
-`;
-
 export const BtnContainer = styled(View)`
   width: 100%;
   margin: 32px 0 32px;
